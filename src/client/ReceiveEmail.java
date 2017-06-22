984817c8250289297df380c45cda353b7749a465
HughHuang3805 <hughxq@gmail.com>
2017-06-21 23:38:32 -0400

    added oauth2.0

--- src/client/ReceiveEmail.java
+++ src/client/ReceiveEmail.java
@@ -14,12 +14,57 @@ import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.internet.MimeBodyPart;
 
+import com.sun.mail.imap.IMAPStore;
+
 public class ReceiveEmail{  
 
 	private String host = "imap.gmail.com";
 	private String mailStoreType = "imap";  
 	private String username = "";  
 	private String password = "";
+
+	/*public void receiveEmail() throws Exception{
+		try{
+			IMAPStore imapStore = OAuth2Authenticator.connectToImap("imap.gmail.com",
+                    993,
+                    username,
+                    ,
+                    true);
+			Folder emailFolder = imapStore.getFolder("INBOX");  
+			emailFolder.open(Folder.READ_ONLY);  
+
+			//4) retrieve the messages from the folder in an array and print it  
+			Message[] messages = emailFolder.getMessages();  
+			for (int i = messages.length - 1; i >= 0 ; i--) {  
+				Message message = messages[i];  
+				System.out.println("---------------------------------");  
+				System.out.println("Email Number " + (i + 1));  
+				System.out.println("Subject: " + message.getSubject());  
+				System.out.println("From: " + message.getFrom()[0]);  
+				Scanner s;
+				s = new Scanner(message.getInputStream()).useDelimiter("\\A");
+				String result = s.hasNext() ? s.next() : " ";
+				System.out.println(result);
+				//encryptedEmail.println(result);  
+				//encryptedEmail.close();
+
+				getMultipart(message);
+				TestBCOpenPGP x = new TestBCOpenPGP();
+				x.decrypt();//once the cipher-text.dat is downloaded, start to decrypt
+			}  
+
+			//5) close the store and folder objects  
+			emailFolder.close(false);  
+			imapStore.close();  
+
+		} catch (NoSuchProviderException e){
+			e.printStackTrace();
+		} catch (MessagingException e1){
+			e1.printStackTrace();
+		} catch (IOException e2){
+			e2.printStackTrace();
+		}
+	}*/
 	
 	public void receiveEmail() throws Exception {  
 		try {  
@@ -58,11 +103,11 @@ public class ReceiveEmail{
 				System.out.println(result);
 				//encryptedEmail.println(result);  
 				//encryptedEmail.close();
-				
-				/*getMultipart(message);
+
+				getMultipart(message);
 				TestBCOpenPGP x = new TestBCOpenPGP();
 				x.decrypt();//once the cipher-text.dat is downloaded, start to decrypt
-*/			}  
+			}  
 
 			//5) close the store and folder objects  
 			emailFolder.close(false);  
@@ -71,31 +116,31 @@ public class ReceiveEmail{
 		} catch (NoSuchProviderException e) {e.printStackTrace();}   
 		catch (MessagingException e) {e.printStackTrace();}  
 		catch (IOException e) {e.printStackTrace();}  
-	}  
+	} 
 
 	public void getMultipart(Message message) throws IOException, MessagingException{//download the cipher-text.dat
-        Multipart multiPart = (Multipart) message.getContent();
-        String attachFiles = "";
-        @SuppressWarnings("unused")
+		Multipart multiPart = (Multipart) message.getContent();
+		String attachFiles = "";
+		@SuppressWarnings("unused")
 		String messageContent = "";
-        int numberOfParts = multiPart.getCount();
-        for (int partCount = 0; partCount < numberOfParts; partCount++) {
-            MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
-            if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-                // this part is attachment
-                String fileName = part.getFileName();
-                attachFiles += fileName + ", ";
-                part.saveFile(fileName);
-                //System.out.println(fileName);
-            } else {
-                // this part may be the message content
-                messageContent = part.getContent().toString();
-            }
-        }
+		int numberOfParts = multiPart.getCount();
+		for (int partCount = 0; partCount < numberOfParts; partCount++) {
+			MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(partCount);
+			if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
+				// this part is attachment
+				String fileName = part.getFileName();
+				attachFiles += fileName + ", ";
+				part.saveFile(fileName);
+				//System.out.println(fileName);
+			} else {
+				// this part may be the message content
+				messageContent = part.getContent().toString();
+			}
+		}
 
-        if (attachFiles.length() > 1) {
-            attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
-        }
+		if (attachFiles.length() > 1) {
+			attachFiles = attachFiles.substring(0, attachFiles.length() - 2);
+		}
 	}
 
 	public String getUsername() {
@@ -113,5 +158,5 @@ public class ReceiveEmail{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 } 
